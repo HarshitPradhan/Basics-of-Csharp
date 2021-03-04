dd714316ccd222f972ffbf871726dcf0161c5b36
HarshitPradhan <59720787+HarshitPradhan@users.noreply.github.com>
2021-03-04 09:46:01 +0530

    Add project files.

---
+++ Basics of Csharp/myclass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Csharp
+{
+
+    class myclass
+    {
+        public static void getstudent()
+        {
+
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "\\TextFile1.txt";
+
+            if (File.Exists(path))
+            {
+                string[] contents = File.ReadAllLines(path);
+                foreach (string content in contents)
+                {
+                    string[] details = content.Split(",");
+                    Console.WriteLine($"Name: {details[0]}, Age: {details[1]}, Dept: {details[2]}");
+                }
+            }
+        }
+    }
+}
